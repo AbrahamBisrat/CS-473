00ab2416d8812beabed5b6de05fa9039049f1189
Abraham Bisrat <abrahammehari0@gmail.com>
2022-11-06 17:54:12 -0600

    Added weekly assignment

---
+++ week4/WalmartLogin/app/src/main/java/edu/miu/walmartlogin/ui/ElectronicsCategoryActivity.kt
@@ -0,0 +1,39 @@
+package edu.miu.walmartlogin.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import edu.miu.walmartlogin.R
+import edu.miu.walmartlogin.adapter.CategoryAdapter
+import edu.miu.walmartlogin.data.Product
+import edu.miu.walmartlogin.databinding.ActivityElectronicsItemBinding
+
+class ElectronicsCategoryActivity : AppCompatActivity(), CategoryAdapter.Callback {
+
+    private lateinit var binding: ActivityElectronicsItemBinding
+    private val productList = mutableListOf(
+        Product("MacBook \" Laptop", "$999", "Matte Black", R.drawable.laptop, 12L, "Nice laptop Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the "),
+        Product("Nokia Lumia\" 64GB android tablet", "$55", "Black", R.drawable.tablet, 123L, "Nice tablet Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the "),
+        Product("Market place\" class 4K", "$235", "Black", R.drawable.tv, 8979L, "Nice TV Screen Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the ."),
+        Product("Cool gadget", "$135", "Black", R.drawable.scanner, 8798L, "Nice Scanner Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the .")
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityElectronicsItemBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = CategoryAdapter(this, productList)
+        adapter.setCallback(this)
+        recyclerView.adapter = adapter
+    }
+
+    override fun onItemClicked(product: Product?) {
+        val intent = Intent(this, ElectronicsDetailActivity::class.java)
+        intent.putExtra("product", product)
+        startActivity(intent)
+    }
+}
